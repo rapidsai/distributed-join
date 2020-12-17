284d72504b7a7ebb1a2d53b488c4b251a8cb607f
esoha-nvidia <69258779+esoha-nvidia@users.noreply.github.com>
2020-12-17 12:38:40 -0700

    Save logs and upload them to AWS (#52)
    Co-authored-by: esoha-nvidia <esoha-nvidia@users.noreply.github.com>

--- ci/ci.sh
+++ ci/ci.sh
@@ -2,9 +2,9 @@
 
 set -eux
 
-env
-
 [ ! -z ${WEBHOOK_URL} ] || (echo "Missing WEBHOOK_URL" && false)
+[ ! -z ${AWS_ACCESS_KEY_ID} ] || (echo "Missing AWS_ACCESS_KEY_ID" && false)
+[ ! -z ${AWS_SECRET_ACCESS_KEY} ] || (echo "Missing AWS_SECRET_ACCESS_KEY" && false)
 
 # Directory in which to do work should be done.
 CI_DIR="${CI_DIR:-$PWD}"
@@ -12,28 +12,42 @@ CI_DIR="${CI_DIR:-$PWD}"
 # Unique ID for each run so that they don't clobber each other.
 CI_ID="$(date +%s%N)"
 
+NOW="$(date -Iseconds)"
+
 function cleanup
 {
     set +ue
+    exec 2>&4 1>&3
     if [ -z "${SUCCESS+x}" ]; then
-        curl -X POST \
-             -H 'Content-type: application/json' \
-             --data '{"text":"<!here> Latest CI errored"}' "${WEBHOOK_URL}"
+        MESSAGE="<!here> Latest CI errored"
+    elif [ -z "${ELAPSED_TIME}" ]; then
+        MESSAGE="<!here> Latest CI failed"
+    else
+        MESSAGE="Latest result: ${ELAPSED_TIME}"
     fi
-    [ ! -z ${SUCCESS}EBHOOK_URL} ] || (echo "Missing WEBHOOK_URL" && false)
+    curl -X POST \
+         -H 'Content-type: application/json' \
+         --data "{\"blocks\":[{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"${MESSAGE}\"}},{\"type\":\"section\",\"text\":{\"type\":\"mrkdwn\",\"text\":\"distributed-join CI log\"},\"accessory\":{\"type\":\"button\",\"text\":{\"type\":\"plain_text\",\"text\":\"View\",\"emoji\":true},\"url\":\"https://dtcomp-data-analytics-ci.s3.amazonaws.com/ci/distributed-join-ci-$NOW.txt\",\"action_id\":\"button-action\"}}]}" "${WEBHOOK_URL}"
+    aws s3 cp "${CI_DIR}/${CI_ID}/distributed-join-ci-${NOW}.txt" s3://dtcomp-data-analytics-ci/ci/
     conda deactivate
     conda env remove -y --name "join-ci-$CI_ID"
     rm -rf "${CI_DIR}/${CI_ID}"
 }
 trap cleanup EXIT
 
-mkdir "${CI_DIR}/${CI_ID}"
 source "${CONDA_PREFIX}/etc/profile.d/conda.sh"
+
+mkdir "${CI_DIR}/${CI_ID}"
+# Logging as suggested here: https://serverfault.com/a/103569
+exec 3>&1 4>&2
+exec 1>"${CI_DIR}/${CI_ID}/distributed-join-ci-$NOW.txt" 2>&1
+
 conda create -y --name "join-ci-${CI_ID}"
 set +u && conda activate "join-ci-${CI_ID}" && set -u
 
 module load cuda/11.0.3
 
+set +u && conda install -c conda-forge awscli && set -u
 set +u && conda install -y -c rapidsai-nightly -c nvidia -c conda-forge -c defaults cudf=0.17 python=3.8 cudatoolkit=11.0 && set -u
 set +u && conda install -y -c rapidsai-nightly -c nvidia -c conda-forge -c defaults ucx ucx-proc=*=gpu nccl && set -u
 
@@ -74,15 +88,7 @@ export ELAPSED_TIME=$(UCX_MEMTYPE_CACHE=n \
 popd
 popd
 
-if [ -z "${ELAPSED_TIME}" ]; then
-    curl -X POST \
-         -H 'Content-type: application/json' \
-         --data '{"text":"<!here> Latest CI failed"}' "${WEBHOOK_URL}"
-else
-    curl -X POST \
-         -H 'Content-type: application/json' \
-         --data "{\"text\":\"Latest result: ${ELAPSED_TIME}\"}" "${WEBHOOK_URL}"
-fi
-
 export SUCCESS=1
 echo done
+
+exit 0
